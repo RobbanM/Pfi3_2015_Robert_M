6e5ecbbb39a5a8e3d9a77287298b69415bee9170
Robert <woot140@hotmail.com>
2015-04-14 16:00:45 +0200

    Finished

--- assignment_1/src/main/java/com/robert/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/robert/assignment_1/MainActivity.java
@@ -1,25 +1,69 @@
 package com.robert.assignment_1;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
 
+import java.util.Random;
+
+
+
+import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
+
+
+    public void buttonClick(View v){
+
+        Random rand = new Random();
+
+        TextView txtView = (TextView) findViewById(R.id.quote_field);
+        txtView.setText(null);
+
+        String[] quoteList= getResources().getStringArray(R.array.quote);
+
+        int nextQuote = rand.nextInt(quoteList.length);
+        txtView.setText(quoteList[nextQuote]);
+        Log.i("MyAPP","Button pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_main);}
+
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        Log.i("onStart", "Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume", "Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause", "Paused");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy", "Destroyed");
     }
 
     @Override
